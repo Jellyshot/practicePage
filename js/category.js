778277e2d003adb42236e90e160f7f4b0b08739f
Jellyshot <94811669+Jellyshot@users.noreply.github.com>
2021-12-13 03:46:51 +0900

    2021-12-13 03:46
    3page.....

---
+++ js/category.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function myFunction(){
+    let x = document.getElementById("search-txt");
+
+    if(x.style.display === 'none'){
+        x.style.display = 'block';
+    } else{
+        x.style.display = 'none';
+    }
+}
